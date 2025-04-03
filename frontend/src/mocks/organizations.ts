6e9efe115d91573226db5774efdf30bddd855638
Yu Fei Yang <ap665539@gmail.com>
2025-04-04 07:56:50 +0800

    更新 mockOrganizations

--- frontend/src/mocks/organizations.ts
+++ frontend/src/mocks/organizations.ts
@@ -1,30 +1,30 @@
 export const mockOrganizations = [
     {
-      organization_id: '#ORG01',
+      organization_id: 'ORG001',
       organization_name: 'Managerial',
     },
     {
-      organization_id: '#ORG02',
+      organization_id: 'ORG002',
       organization_name: 'Design',
     },
     {
-      organization_id: '#ORG03',
+      organization_id: 'ORG003',
       organization_name: 'Engineering',
     },
     {
-      organization_id: '#ORG04',
+      organization_id: 'ORG004',
       organization_name: 'Marketing',
     },
     {
-      organization_id: '#ORG05',
+      organization_id: 'ORG005',
       organization_name: 'HR',
     },
     {
-      organization_id: '#ORG06',
+      organization_id: 'ORG006',
       organization_name: 'Data Science',
     },
     {
-      organization_id: '#ORG07',
+      organization_id: 'ORG007',
       organization_name: 'Sales',
     },
   ];
