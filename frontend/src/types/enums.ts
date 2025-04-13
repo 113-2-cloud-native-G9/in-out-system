24c4c3546da1715b7fbbd395439f5875d55bd818
Vicky Yang <ap665539@gmail.com>
2025-04-13 20:28:40 +0800

    Frontend dev (#4)
    * Refactor .gitignore
    
    * Initialize frontend settings
    
    * 忘了放lib
    
    * Add routes&pages
    
    * Update Menu
    
    * Delete .vite/deps
    
    * Add LoginPage
    
    * css setting
    
    * Update
    
    * Refactor menu component
    
    * Update org management
    
    * Update EditEmployee
    
    * Up up up up
    
    * AttendancePage Update
    
    * access log component
    
    * menu fixed and accesslog adjust
    
    * 串登入API 改很多東東
    
    * 更新 mockOrganizations
    
    * Add ChangePassword
    
    * Fontend Dockfile
    
    * upload cicd workflow
    
    ---------
    
    Co-authored-by: r12725065 <r12725065@ntu.edu.tw>

---
+++ frontend/src/types/enums.ts
@@ -0,0 +1,23 @@
+export enum HttpMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+}
+
+export enum LateArrivalStatus {
+    OnTime = "On time",
+    Late = "Late",
+}
+
+export enum EarlyDepartureStatus {
+    OnTime = "On time",
+    Early = "Early",
+}
+
+export enum AttendanceStatus {
+    All = "All",
+    OnTime = "On time",
+    Late = "Late",
+    Early = "Early departure",
+}
