ab839744f0a45872dbdaec8fb6f986ea7039935a
Vicky Yang <ap665539@gmail.com>
2025-04-13 22:32:18 +0800

    Backend dev (#9)
    * Change file arch.
    
    * update backend.yml arch.
    
    * update
    
    * Update deploy.yml
    
    * test backend-dev run on gcp
    
    * push backend to gcp
    
    * test gcp2
    
    * test gcp3
    
    * trigger workflow
    
    * trigger Cloud Run deploy after enabling SQL API
    
    * test gcp
    
    * update
    
    * Merge branches 'backend-dev' and 'backend-dev' of https://github.com/113-2-cloud-native-G9/in-out-system into backend-dev
    
    * update
    
    ---------
    
    Co-authored-by: yueh-sheng hou <danny890802@gmail.com>

--- frontend/src/components/custom/spinner.tsx
+++
@@ -1,6 +0,0 @@
-
-const Spinner = () => (
-    <div className="w-6 h-6 border-4 border-t-4 border-primary rounded-full animate-spin"></div>
-);
-
-export default Spinner;
--- frontend/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export type * from "./user.d.ts";
-export type * from "./attendance.d.ts";
-export type * from "./accesslog.d.ts";
-export * from "./enums.ts";
