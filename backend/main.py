fb24f8524e93c6541339194dffcf39bcb4076990
jushengtin2 <danny890802@gmail.com>
2025-05-08 01:03:20 +0800

    tsest

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app(config_name="production")    #要換模式就來這邊換 本機開發-> development , 記得push的時候要改production
+app = create_app(config_name="development")    #要換模式就來這邊換 本機開發-> development , 記得push的時候要改production
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,16 +1,11 @@
 import pytest
 from app import create_app
 from app.models import db
-import os
 
 @pytest.fixture
 def client():
     app = create_app() 
-    os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    app = create_app()
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
-    
 
     with app.app_context():
         db.create_all()  
